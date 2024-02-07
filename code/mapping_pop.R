a9e9402f26d7ee4eca3f342f34c7ae72763b978b
dopapus <papaidoops@gmail.com>
2024-02-07 19:35:38 +0000

    updated mapping script

--- code/mapping_pop.R
+++ code/mapping_pop.R
@@ -1,4 +1,4 @@
-
+# Mapping the admin-levels and population data
 
 sf_use_s2(FALSE)
 tm_shape(mlw_shp) + tm_fill() + tm_borders()
@@ -7,28 +7,57 @@ tm_shape(zam_shp) + tm_fill() + tm_borders()
 
 tm_shape(zim_shp) + tm_fill() + tm_borders()
 
-# crop the population data to MLW_clusters shapefile and local municipalities
-pop.mlw_clusters <- crop(pop_u5, dat)
-popu5.mlw_clusters <- mask(pop.mlw_clusters, dat)
+# crop the population data to shapefile and local municipalities
+# Malawi
+pop_mlw_shp <- crop(mlw_pop, mlw_shp)
+pop_mlw     <- mask(pop_mlw_shp, mlw_shp)
 
-pop.blantyre <- crop(pop_u5, blantyre )
-popu5.blantyre <- mask(pop.blantyre, blantyre)
+# Zambia
+pop_zam_shp <- crop(zam_pop, zam_shp)
+pop_zam     <- mask(pop_zam_shp, zam_shp)
+
+# Zimbabwe
+pop_zim_shp <- crop(zim_pop, zim_shp)
+pop_zim     <- mask(pop_zim_shp, zim_shp)
 
 # convert into spatial pixel data frame then into data frame to be able to plot in ggplot
 # check population size
-u5_mlw_spdf <- as(popu5.mlw_clusters, "SpatialPixelsDataFrame")
-u5_mlw_df <- as.data.frame(u5_mlw_spdf)
-colnames(u5_mlw_df) <- c("value", "x", "y")
-sum(u5_mlw_df$value) # 111,856
+# Malawi
+pop_mlw_spdf <- as(pop_mlw, "SpatialPixelsDataFrame")
+mlw_df <- as.data.frame(pop_mlw_spdf)
+colnames(mlw_df) <- c("value", "x", "y")
+sum(mlw_df$value) # 111,856
 
+# Zambia
+pop_zam_spdf <- as(pop_zam, "SpatialPixelsDataFrame")
+zam_df <- as.data.frame(pop_zam_spdf)
+colnames(zam_df) <- c("value", "x", "y")
+sum(zam_df$value) # 111,856
 
-u5_blan_spdf <- as(popu5.blantyre, "SpatialPixelsDataFrame")
-u5_blan_df <- as.data.frame(u5_blan_spdf)
-colnames(u5_blan_df) <- c("value", "x", "y")
-sum(u5_blan_df$value) # 141,545
+# Zimbabwe
+pop_zim_spdf <- as(pop_mlw, "SpatialPixelsDataFrame")
+zim_df <- as.data.frame(pop_zim_spdf)
+colnames(zim_df) <- c("value", "x", "y")
+sum(zim_df$value) # 111,856
 
+# plotting ----
+# Malawi
 ggplot()  +
     geom_sf(data = blantyre) +
+    geom_tile(data = u5_blan_df[u5_blan_df$value>0,], aes(x=x, y=y, fill=value), alpha=0.6) +
+    scale_fill_viridis() +
+    geom_sf(data = dat, alpha = 0.1, lwd = 0.5)
+
+# Zambia
+ggplot()  +
+    geom_sf(data = blantyre) +
+    geom_tile(data = u5_blan_df[u5_blan_df$value>0,], aes(x=x, y=y, fill=value), alpha=0.6) +
+    scale_fill_viridis() +
+    geom_sf(data = dat, alpha = 0.1, lwd = 0.5)
+
+# Zimbabwe
+ggplot()  +
+geom_sf(data = blantyre) +
     geom_tile(data = u5_blan_df[u5_blan_df$value>0,], aes(x=x, y=y, fill=value), alpha=0.6) +
     scale_fill_viridis() +
     geom_sf(data = dat, alpha = 0.1, lwd = 0.5)
